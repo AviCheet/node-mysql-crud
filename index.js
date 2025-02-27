b97fc6ef3bc3f2c8b01d4ee03ff21f82a0a77c40
Avinash Cheetireala <avinashcheetireala@Mac.lan>
2025-02-27 14:00:16 -0500

    the updated code after PORT changes

--- index.js
+++ index.js
@@ -1,39 +1,8 @@
-// const express = require('express'),
-//     app = express();
-//     bodyparser = require('body-parser');
-// require('express-async-errors')
-
-
-// const bodyParser = require('body-parser');
-// const db = require('./db'),
-//     employeeRoutes = require('./controller/employee.controller')
-
-// //middleware
-// app.use(bodyparser.json())
-// app.use('/api/employees',employeeRoutes)
-
-// app.use((err,req,res,next) => {
-//     console.log(err)
-//     res.status(err.status || 500).send('Something went wrong')
-// })
-
-
-// //make sure db connection is successful
-// // then start the express server
-// db.query("SELECT 1")
-// .then(() => {
-//     console.log('db connection is successfull')
-//     app.listen(3000,
-//         () => console.log('server started at 3000'))
-// })
-// .catch(err => console.log('db connection failed. \n' . err))
-
-
-const express = require('express');
+//const express = require('express');
 const app = express();
 const db = require('./db');
 const employeeRoutes = require('./controller/employee.controller');
-require('express-async-errors');
+//require('express-async-errors');
 
 // Middleware
 app.use(express.json()); // Use express's built-in JSON parser
